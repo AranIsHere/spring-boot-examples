f83a873e5eaf236e729fbdb228cf2f1ac49b72af
AranIsHere <343081716@qq.com>
2020-07-13 23:58:14 +0800

    first commit

---
+++ springBoot-aop/src/test/java/com/aran/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aran.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
